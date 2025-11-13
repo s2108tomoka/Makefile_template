43c4edcf9a80ba92c47d2abda125b5e40b24e2d6
s2108 <s2108.ochitomoka@gmail.com>
2025-11-13 15:41:01 +0900

    add cpp Makefile template

---
+++ cpp_make.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# make.sh
+CPP_FILES=$(ls | grep '\.cpp$' | sed 's/$/ \\/')
+
+cat <<EOF > Makefile
+CPP=c++
+CFLAGS=-Wall -Wextra -Werror -std=c++98 -pedantic
+NAME=ex01
+FILES= \\
+$CPP_FILES
+
+INCLUDES= Zombie.hpp
+
+OBJS=\$(FILES:.cpp=.o)
+
+all: \$(NAME)
+
+\$(NAME): \$(OBJS)
+	\$(CPP) \$(CFLAGS) -o \$(NAME) \$(OBJS)
+
+%.o: %.cpp \$(INCLUDES)
+	\$(CPP) \$(CFLAGS) -c \$< -o \$@
+
+clean:
+	\$(RM) \$(OBJS)
+
+fclean: clean
+	\$(RM) \$(NAME)
+
+re:
+	@\$(MAKE) fclean
+	@\$(MAKE) all
+
+.PHONY: all clean fclean re
+EOF
+
+echo "✅ Create Makefile"
